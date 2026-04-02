1d0fee01a525a6a140a282b1e54bf5505f486425
SNDMN512 <lyabinnews@mail.ru>
2026-04-02 18:45:35 +0500

    modified script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "Git";
+const screens = 4;
+let screenPrice = 15.44;
+let rollback = 3;
+let fullPrice = 20.25;
+let adaptive = true;
+
+alert("learning Git");
+console.log("learning Git");
