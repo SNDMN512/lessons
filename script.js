daf2e9e79e20f9f1f0640b06a1494659caff0e86
Yuriy L. <lyabinnews@mail.ru>
2026-04-03 15:12:24 +0500

    code revision

--- script.js
+++ script.js
@@ -10,11 +10,19 @@ let service2 = prompt("Какой дополнительный тип услуг
 let servicePrice2 = +prompt("Сколько это будет стоить?");
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = fullPrice > 30000 ? Math.ceil(fullPrice - fullPrice/100*10) : fullPrice <= 15000 && fullPrice >= 30000 ? Math.ceil(fullPrice - fullPrice/100*5) : fullPrice;
-console.log(servicePercentPrice);
+
 
 fullPrice > 30000 ? console.log("Даем скидку в 10%") : fullPrice <= 15000 && fullPrice >= 30000 ? console.log("Даем скидку в 5%") : 
 fullPrice > 0 && fullPrice < 15000 ? console.log("Скидка не предусмотрена") : console.log("Что-то пошло не так");
 
+
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(servicePercentPrice);
+console.log(`Стоимость вёрстки экрана ${screenPrice}$`);
 
 
 
