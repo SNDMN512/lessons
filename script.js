41b1905e1ef24be1d00231f50dcdd353ea9fe309
Yuriy L. <lyabinnews@mail.ru>
2026-04-04 11:04:55 +0500

    add functions

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
 "use strict";
 
-let title = prompt("Как называется ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+let service;
+let servicePrice;
+
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+const asking = function () {
+    title = prompt("Как называется ваш проект?", "Калькулятор вёрстки")
+    screens = prompt("Какие типы экранов нужно разработать?", "Простые, сложные, комбинированные")
+    screenPrice = prompt("Сколько будет стоить данная работа?")
+
+    while (!isNumber(screenPrice)) {
+        screenPrice = prompt("Сколько будет стоить данная работа?")
+    }
+
+    adaptive = confirm("Нужен ли адаптив на сайте?")
+}
+
+const getAllServicePrices = function () {
+    let sum = 0
+        
+    for (let i = 0; i < 2; i++) {
+        service = prompt("Какой дополнительный тип услуги нужен?")
+        servicePrice = prompt("Сколько это будет стоить?")
+        while (!isNumber(servicePrice)) {
+            prompt("Сколько это будет стоить?")
+        }
+    sum += Number(servicePrice)
+    }
+    return sum
+}
+    
+const getFullPrice = function () {
+    return Number(screenPrice) + allServicePrices
+}
+
+const getServicePercentPrice = function (price) {
+    if (price > 30000) {
+        return price - price/100*10
+    } else if (price >= 15000 && price <= 30000) {
+        return price - price/100*5
+    } else if (price >= 0 && price < 15000) {
+        return price
+    } else {
+        return "Что то пошло не так";
+    }
+}
+
+const getTitle = function () {
+    const titleUpTrim = title.trim()[0].toUpperCase() + title.trim().slice(1).toLocaleLowerCase()
+    return titleUpTrim;
+}
 
 const showTypeof = function(variable) {
     return `${variable} ${typeof variable}`;
@@ -24,41 +76,19 @@ const getRollbackMessage = function(price) {
     } else {
     return "Что-то пошло не так"
     }
-
 }
 
-const allServicePrices = function (price1, price2) {
-    return price1 + price2
-}
-    
-const getAllServicePrices = allServicePrices(servicePrice1, servicePrice2);
+asking();
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrice(fullPrice);
+title = getTitle();
 
-const getFullPrice = function () {
-    return screenPrice + getAllServicePrices
-}
-
-const fullPrice = getFullPrice(screenPrice, getAllServicePrices);
-
-const getTitle = function (str) {
-    const titleUpTrim = str.trim()[0].toUpperCase() + str.trim().slice(1).toLocaleLowerCase()
-    return titleUpTrim;
-}
-
-const getServicePercentPrices = function (price) {
-    if (price > 30000) {
-        return price - price/100*10
-    } else if (price >= 15000 && price <= 30000) {
-        return price - price/100*5
-    } else if (price >= 0 && price < 15000) {
-        return price
-    } else {
-        return "Что то пошло не так";
-    }
-}
+showTypeof(title);
 
 console.log(`Тип данных: ${showTypeof(title)}`);
-console.log(getTitle(title));
+console.log(title);
 console.log(`Стоимость вёрстки и дополнительных услуг без учёта скидки: ${fullPrice}`);
-console.log(`Стоимость дополнительных услуг: ${getAllServicePrices}`);
+console.log(`Стоимость дополнительных услуг: ${allServicePrices}`);
 console.log(getRollbackMessage(fullPrice));
-console.log(`Стоимость услуг с учётом скидки: ${getServicePercentPrices(fullPrice)}`);
+console.log(`Стоимость услуг с учётом скидки: ${servicePercentPrice}`);
